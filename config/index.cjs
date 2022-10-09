7523f36822fa0241880a9a04ffe439c21c61c152
pengyuazz <115406538+pengyuazz@users.noreply.github.com>
2022-10-09 22:17:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oUchI6wRDWIfaneD9P3lKm8Toeqo',
+      id: 'oUchI64ZMQbRHC1p0Xj6qoOm04KI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'bKYlhxfbrA0-VZF81tXGVuzo5xD3iWniXU3MWfxX624',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
