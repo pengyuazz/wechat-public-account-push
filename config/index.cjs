1e1e4ec680f74384f2c622408a82765b56ab944a
pengyuazz <115406538+pengyuazz@users.noreply.github.com>
2022-10-09 22:12:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oUchI6wRDWIfaneD9P3lKm8Toeqo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'nx3zADnCdJBcHiOl4ZLl2PL7PEbmFS-Vk9IF8IEeGdE',
+      useTemplateId: 'bKYlhxfbrA0-VZF81tXGVuzo5xD3iWniXU3MWfxX624',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-27',
       festivals: [
